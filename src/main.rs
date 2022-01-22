0f3cfad8192b2baa761af5289736054d259cb3ea
Matt Gleich <git@mattglei.ch>
2022-01-22 16:19:06 -0500

    feat: port to rppal
    Signed-off-by: Matt Gleich <git@mattglei.ch>

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,23 @@
-use std::{thread, time};
+use std::{thread, time::Duration};
 
-use rust_gpiozero::LED;
+use rppal::{gpio::Gpio, system::DeviceInfo};
 
 fn main() {
-    println!("Started program");
-    let led = LED::new(17);
-    led.on();
-    println!("Turned LED on");
-    thread::sleep(time::Duration::from_secs(1));
-    led.off();
-    println!("Turned LED off");
-    led.close();
+    println!(
+        "Blinking an LED on a {}.",
+        DeviceInfo::new()
+            .expect("Failed to get device info")
+            .model()
+    );
+
+    let mut pin = Gpio::new()
+        .expect("Failed to create new pin")
+        .get(17)
+        .expect("Failed to get pin 17")
+        .into_output();
+
+    loop {
+        pin.toggle();
+        thread::sleep(Duration::from_millis(500));
+    }
 }
